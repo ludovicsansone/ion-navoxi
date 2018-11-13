4f69c56963bdb325734b1f0dc9ee84af91008c3a
ludovicsansone <ludovicsansone@Mac-mini-de-ludovic.local>
2018-11-13 22:28:26 +0100

    Update file route.ts

--- src/sncf/components/route/route.ts
+++ src/sncf/components/route/route.ts
@@ -4,14 +4,14 @@ import { TtsProvider } from '../../../providers/tts/tts';
 import { NavController } from 'ionic-angular';
 
 /**
- * Generated class for the RouteComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+* Generated class for the RouteComponent component.
+*
+* See https://angular.io/api/core/Component for more info on Angular
+* Components.
+*/
 @Component({
-  selector: 'route',
-  templateUrl: 'route.html'
+    selector: 'route',
+    templateUrl: 'route.html'
 })
 export class RouteComponent {
     private destination: string = "";
@@ -23,51 +23,52 @@ export class RouteComponent {
     private enableNextStep = true;
     private walking = new Audio("assets/audio/walking.mp3");
 
+    constructor(
+    public nav: NavController,
+    private routeProvider: RouteProvider,
+    private ttsProvider: TtsProvider) { }
 
+    ngOnInit() {
+        this.destination = this.routeProvider.destination;
+        this.quai = this.routeProvider.quai;
+        this.ttsProvider.sayMessage("Je vous emmène jusqu'au quai " + this.quai + ", pour prendre le train en direction de " + this.destination);
+        this.route = this.routeProvider.getRoute(this.quai);
+        this.onNextStep();
+    }
 
+    onRepeat() {
+        this.ttsProvider.sayMessage(this.step.instruction);
+    }
 
-  constructor(
-  public nav: NavController,
-  private routeProvider: RouteProvider,
-  private ttsProvider: TtsProvider) { }
+    onNextStep() {
+        let maxIndex = this.route.steps.length - 1;
+        let timeout = 0;
 
-  ngOnInit() {
-      this.destination = this.routeProvider.destination;
-      this.quai = this.routeProvider.quai;
-      this.ttsProvider.sayMessage("Je vous emmène jusqu'au quai " + this.quai + ", pour prendre le train en direction de " + this.destination);
-      this.route = this.routeProvider.getRoute(this.quai);
-      this.onNextStep();
-  }
-
-  onRepeat() {
-      this.ttsProvider.sayMessage(this.step.instruction);
-  }
-
-  onNextStep() {
-      let maxIndex = this.route.steps.length - 1;
-      this.imgSrc = 'assets/imgs/shoes.gif';
-      this.enableNextStep = false;
-
-      if (this.routeIndex < maxIndex) {
-if (this.routeIndex > -1)
-          this.walking.play();
-          setTimeout(() => {
-            this.imgSrc = 'assets/imgs/beacon.png';
-            this.routeIndex += 1;
-            this.step = this.routeProvider.getStep(this.routeIndex);
-            this.ttsProvider.sayMessage(this.step.instruction);
-            this.enableNextStep = true;
-          }, 6000);
-      }
-      else {
-        this.ttsProvider.sayMessage("Vous êtes déjà arrivé");
-        this.imgSrc = 'assets/imgs/beacon.png';
+        this.imgSrc = 'assets/imgs/shoes.gif';
         this.enableNextStep = false;
-      }
-  }
 
-  onReturn() {
-      this.nav.pop();
-  }
+        if (this.routeIndex < maxIndex) {
+            if (this.routeIndex > -1) {
+                this.walking.play();
+                timeout = 6000;
+            }
+            setTimeout(() => {
+                this.imgSrc = 'assets/imgs/beacon.png';
+                this.routeIndex += 1;
+                this.step = this.routeProvider.getStep(this.routeIndex);
+                this.ttsProvider.sayMessage(this.step.instruction);
+                this.enableNextStep = true;
+            }, timeout);
+        }
+        else {
+            this.ttsProvider.sayMessage("Vous êtes déjà arrivé");
+            this.imgSrc = 'assets/imgs/beacon.png';
+            this.enableNextStep = false;
+        }
+    }
+
+    onReturn() {
+        this.nav.pop();
+    }
 
 }
